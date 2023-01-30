e43fbbcb6ed669f574e2a2ff69237201fe45e540
thehuseyinoglu <thehuseyinoglu@gmail.com>
2023-01-31 00:51:17 +0300

    Create likesSlice.js

---
+++ redux/features/likes/likesSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    likes: []
+}
+
+export const likesSlice = createSlice({
+    name: 'likes',
+    initialState,
+    reducers: {
+        likeProduct: (state, action) => {
+            state.likes.push(action.payload)
+        },
+        unLikeProduct: (state, action) => {
+            state.likes = state.likes.filter(i => i !== action.payload)
+        },
+        deleteLikes: (state) => {
+            state.likes = []
+        }
+
+    }
+})
+
+export const { likeProduct, unLikeProduct,deleteLikes } = likesSlice.actions
+export default likesSlice.reducer
